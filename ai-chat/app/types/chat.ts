c78a7b6230984c688109ce27dc0c1de1465f6bf5
Mohamed Anwar <mohamedanwar@MacBook-Air--Mohamed-Anwar.local>
2026-09-12 18:36:31 +0300

    Initial AI Chat project

---
+++ ai-chat/app/types/chat.ts
@@ -0,0 +1,9 @@
+export type MessageRole = "user" | "assistant";
+
+export type Message  = {
+    id: string;  
+    role: MessageRole;
+    content: string;
+    createdAt?: Date;
+    avatar?: string;
+}
